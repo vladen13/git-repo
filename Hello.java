5db652d187f28f51e6f477f236fdc9167b86427d
vladen13 <vladyslav.denysenko13@gmail.com>
2017-01-21 18:25:04 +0200

    click clack

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello world!");
-		System.out.print("Hello everyone!");
-		System.out.print("Wazzup kid?");
+		System.out.println("Hello world;");
+		System.out.print("Hello everyone;");
+		System.out.print("Wazzup kid;");
 	}
 }
