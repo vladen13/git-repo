ed0d973fd726486d71bc66c165555f8f81b8099a
vladen13 <vladyslav.denysenko13@gmail.com>
2017-01-21 18:06:36 +0200

    Write code

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello world!");
 		System.out.print("Hello everyone!");
+		System.out.print("Wazzup kid?");
 	}
 }
