b68af67ebf6e9fe65cdae282fd6a18edb7028581
vladen13 <vladyslav.denysenko13@gmail.com>
2017-01-21 16:08:28 +0200

    Create class for the everyone greatting.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.print("Hello world!");
+		System.out.println("Hello world!");
+		System.out.print("Hello everyone!");
 	}
 }
